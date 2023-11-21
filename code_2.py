ac8dec22183f21d31bab2839d033b34b5aaa1887
nuloparew <144057022+nuloparew@users.noreply.github.com>
2023-11-21 07:39:52 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+
+
+    return data
+def generate_random_data():
+import random
+
+    for item in data:
+    main()
+def main():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
